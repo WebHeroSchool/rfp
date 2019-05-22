499cd6dc1198f82c558046e8026724c889afa168
Victor Zhuravlev <mocambe@mail.ru>
2019-05-22 19:19:40 +0300

    lesson 19 completed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import './Footer.css';
 
 const Footer = ({ todoCount }) => (
-    <footer>Number of remaining todos: {todoCount}</footer>
+    <footer className='footer'>Number of remaining todos: {todoCount}</footer>
 );
 
 export default Footer;
