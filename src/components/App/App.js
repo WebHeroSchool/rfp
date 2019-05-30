7077091ea3b987ba7841679a51ff7ddec98c7db1
Victor Zhuravlev <mocambe@mail.ru>
2019-05-30 18:59:49 +0300

    lesson 25 completed

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,40 +1,46 @@
 import React from 'react';
 import Footer from '../Footer/Footer';
 import ItemList from '../ItemList/ItemList';
-import styles from './App.module.css';
 import InputItem from '../InputItem/InputItem';
 import RadioFilters from '../RadioFilters/RadioFilters';
 
-const App = () => {
-const todoItems = [
-    {
-        value: 'Important',
-        isDone: false,
-    }, 
-    {
-        value: 'Urgent',
-        isDone: false,
-    }, 
-    {
-        value: 'Miscellaneous',
-        isDone: true,
-    },
-    {
-        value: 'The rest',
-        isDone: true,
+import styles from './App.module.css';
+
+
+class App extends React.Component {
+    render() {
+        const todoItems = [
+            {
+                value: 'Important',
+                isDone: false,
+            }, 
+            {
+                value: 'Urgent',
+                isDone: false,
+            }, 
+            {
+                value: 'Miscellaneous',
+                isDone: true,
+            },
+            {
+                value: 'The rest',
+                isDone: true,
+            }
+        ];
+        
+            return (
+            <div className={styles.wrapper}>
+                <h1 className={styles.heading}>Todo list</h1>
+                <InputItem />
+                <RadioFilters />
+                <ItemList todoItems = { todoItems }/>
+                <Footer todoCount = {4} />
+        
+            
+            </div>
+        )
     }
-];
+};
 
-    return (
-    <div className={styles.wrapper}>
-        <h1 className={styles.heading}>Todo list</h1>
-        <InputItem />
-        <RadioFilters />
-        <ItemList todoItems = { todoItems }/>
-        <Footer todoCount = {4} />
-
-    
-    </div>
-)};
 
 export default App;
