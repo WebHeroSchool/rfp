411cad24ac541944aa2ad4c9181882aa2ed09c79
Victor Zhuravlev <mocambe@mail.ru>
2019-06-04 21:20:22 +0300

    Revert "onClickAdd method added"
    This reverts commit e3be2a8ef8f5bedfd4d5fe766d02eeb322eeb99c.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,6 @@ import Footer from "../Footer/Footer";
 import ItemList from "../ItemList/ItemList";
 import InputItem from "../InputItem/InputItem";
 import RadioFilters from "../RadioFilters/RadioFilters";
-import ButtonAdd from "../ButtonAdd/ButtonAdd";
 
 import styles from "./App.module.css";
 
@@ -33,8 +32,7 @@ class App extends React.Component {
           isDone: true,
           id: 4
         }
-      ],
-      count: 4
+      ]
     };
   }
   
@@ -50,27 +48,18 @@ class App extends React.Component {
     this.setState({ todoItems: newItemList });
   };
 
-  onClickDelete = id => this.setState(state => ({ todoItems: state.todoItems.filter(item => item.id !== id)}));
-
-  onClickAdd = () => this.setState(state => ({
-      todoItems: [
-          ...state.todoItems,
-          {
-              value: 'Innefable me',
-              isDone: false,
-              id: state.count + 1,
-          }
-      ],
-  }))
+  onClickDelete = id => {
+    const index = this.state.todoItems.findIndex(item => item.id === id);
+    this.state.todoItems.splice(index, 1);
+    const newTodoItems = this.state.todoItems;
+    this.setState( {todoItems: newTodoItems});
+  }
 
   render() {
     return (
       <div className={styles.wrapper}>
         <h1 className={styles.heading}>Todo list</h1>
-        <div className={styles['add-form']}>
-            <InputItem />
-            <ButtonAdd onClickAdd={this.onClickAdd}/>
-        </div>
+        <InputItem />
         <RadioFilters />
         <ItemList
           todoItems={this.state.todoItems}
