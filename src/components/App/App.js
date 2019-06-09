49dc74d65e6659ecfdc0a035cc18262da66f032e
Victor Zhuravlev <mocambe@mail.ru>
2019-06-09 14:00:45 +0300

    routing added to App.Lesson 36 completed

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
 import MenuItem from '@material-ui/core/MenuItem';
 import MenuList from '@material-ui/core/MenuList';
 import Paper from '@material-ui/core/Paper';
@@ -10,18 +12,28 @@ import About from '../About/About';
 import styles from './App.module.css';
 
 const App = () => (
-  <div className={styles.wrapper}>
-    <Paper className={styles.sidebar}>
-      <MenuList>
-        <MenuItem>About me</MenuItem>
-        <MenuItem>Tasks</MenuItem>
-        <MenuItem>Contacts</MenuItem>
-      </MenuList>
-    </Paper>
-    <Paper className={styles.main}>
-      <TodoList />
-    </Paper>
-  </div>
+  <Router>
+    <div className={styles.wrapper}>
+      <Paper className={styles.sidebar}>
+        <MenuList>
+          <Link to='/' className={styles.link}>
+            <MenuItem>About me</MenuItem>
+          </Link>
+          <Link to='/todo' className={styles.link}>
+            <MenuItem>Tasks</MenuItem>
+          </Link>
+          <Link to='/contacts' className={styles.link}>
+            <MenuItem>Contacts</MenuItem>
+          </Link>
+        </MenuList>
+      </Paper>
+      <Paper className={styles.main}>
+        <Route path='/' exact component={About} />
+        <Route path='/todo' component={TodoList} />
+        <Route path='/contacts' component={Contacts} />
+      </Paper>
+    </div>
+  </Router>
 );
 
 export default App;
