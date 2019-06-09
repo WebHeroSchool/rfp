ab99ae4375f5041214865f8dd6ebc71e14ce0aab
Victor Zhuravlev <mocambe@mail.ru>
2019-06-09 23:55:23 +0300

    Refactoring: About changed to class, octokit installed

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
-const About = () => <div>About</div>;
+class About extends React.Component {
+  constructor() {
+    super();
 
+    this.state = {
+      isLoading: false
+    };
+  }
+
+  render() {
+    const { isLoading } = this.state;
+
+    return (
+      <div className=''>
+        <h1>{isLoading ? <CircularProgress /> : 'About me'}</h1>
+      </div>
+    );
+  }
+}
 export default About;
