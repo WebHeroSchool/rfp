004e8ce6b9d0f2aede7c159c903bcc94755fce87
Victor Zhuravlev <mocambe@mail.ru>
2019-06-12 14:59:14 +0300

    wrong login changed in About

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -23,7 +23,7 @@ class About extends React.Component {
   componentDidMount() {
     octokit.repos
       .listForUser({
-        username: 'v1valasveganss'
+        username: 'v1valasvegan'
       })
       .then(({ data }) => {
         console.log(data);
